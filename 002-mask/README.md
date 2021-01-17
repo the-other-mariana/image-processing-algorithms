b589a1cb0de36adfbadcf0ff7153348a01ea7491
Mariana Avalos <mariana.avalos.arce@gmail.com>
2021-01-16 20:47:11 -0600

    updated mask folder

---
+++ 002-mask/README.md
@@ -0,0 +1,17 @@
+# Image Mask
+
+This is a small exercise where a binary filter is used as a mask of visibility of an image in another image. <br />
+
+## Output
+
+The resulting mask is the following. Note that the mask must be the same as the destination image. <br />
+
+![image](https://github.com/the-other-mariana/image-processing-algorithms/blob/master/002-mask/mask.png?raw=true) <br/>
+
+While the image that will be inserted according to mask values is below. <br />
+
+![image](https://github.com/the-other-mariana/image-processing-algorithms/blob/master/002-mask/pattern.png?raw=true) <br/>
+
+The resulting image is the following. Note that the pattern above is in the white areas of the mask. <br />
+
+![image](https://github.com/the-other-mariana/image-processing-algorithms/blob/master/002-mask/result.png?raw=true) <br/>
--- 002-mask/mask.cpp
+++ 002-mask/mask.cpp
@@ -8,15 +8,17 @@ using namespace std;
 using namespace cv;
 
 int main() {
-	Mat original = imread("dados.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-	Mat mask = imread("dados.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-	Mat result = imread("dados.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-	Mat pattern = imread("pattern.png", CV_LOAD_IMAGE_GRAYSCALE);
+	Mat original = imread("dice.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	Mat mask = imread("dice.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	Mat result = imread("dice.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	Mat pattern = imread("pattern.jpg", CV_LOAD_IMAGE_GRAYSCALE);
 
-	//crear mascara: filtro binary
+	int thresh_value = 200;
+
+	//create mask: binary filter
 	for (int i = 0; i < mask.rows; i++) {
 		for (int j = 0; j < mask.cols; j++) {
-			if (mask.at<uchar>(i, j) > 200) {
+			if (mask.at<uchar>(i, j) > thresh_value) {
 				mask.at<uchar>(i, j) = (uchar)(255);
 			}
 			else {
@@ -32,9 +34,11 @@ int main() {
 			}
 		}
 	}
-	namedWindow("mask", WINDOW_AUTOSIZE);
-	imshow("mask", mask);
-	namedWindow("result", WINDOW_AUTOSIZE);
-	imshow("result", result);
+	namedWindow("Mask", WINDOW_AUTOSIZE);
+	imshow("Mask", mask);
+	namedWindow("Result", WINDOW_AUTOSIZE);
+	imshow("Result", result);
+	namedWindow("Pattern", WINDOW_AUTOSIZE);
+	imshow("Pattern", pattern);
 	waitKey(0);
 }
